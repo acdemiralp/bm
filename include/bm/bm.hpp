58de27e9da0e632e2f9bf37f316cf272e78e6088
Ali Can Demiralp <demiralpali@gmail.com>
2019-02-28 15:57:14 +0100

    Added MPI support. Define BM_MPI_SUPPORT and use bm::run_mpi instead of bm::run.

--- include/bm/bm.hpp
+++ include/bm/bm.hpp
@@ -5,79 +5,147 @@
 #include <chrono>
 #include <cmath>
 #include <cstddef>
+#include <cstdint>
 #include <fstream>
 #include <functional>
 #include <limits>
 #include <numeric>
+#include <sstream>
 #include <string>
 #include <vector>
+
+#ifdef BM_MPI_SUPPORT
+#include <mpi.h>
+#endif
 
 namespace bm
 {
 template <typename type = double>
 struct record
 {
-  type mean              () 
+  type        mean              ()                             
   {
     return std::accumulate(values.begin(), values.end(), type(0)) / values.size();
   }
-  type variance          ()
+  type        variance          ()                            
   {
     auto m = mean();
     std::vector<type> differences(values.size());
     std::transform(values.begin(), values.end(), differences.begin(), [m] (const type& value) { return value - m; });
     return std::inner_product(differences.begin(), differences.end(), differences.begin(), type(0)) / values.size();
   }
-  type standard_deviation()
+  type        standard_deviation()                            
   {
     return std::sqrt(variance());
   }
-
-  void to_csv            (const std::string& filepath)
+                                                              
+  std::string to_string         ()                            
   {
-    std::ofstream file(filepath);
-    file.precision(std::numeric_limits<type>::max_digits10);
-    
-    auto size = values.size();
-    for (auto j = 0; j < size; ++j)
-    {
-      file << values[j];
-      if (j != size - 1) 
-        file << ", ";
-    }
+    std::ostringstream stream;
+    stream.precision(std::numeric_limits<type>::max_digits10);
+    stream << name << ",";
+    for (auto& value : values)
+      stream << value << ",";
+    stream << mean() << "," << variance() << "," << standard_deviation();
+    return stream.str();
+  }
+  void        to_csv            (const std::string& filepath) 
+  {
+    std::ofstream stream(filepath);
+    stream << "name,";
+    for (auto i = 0; i < values.size(); ++i)
+      stream << "iteration " << i << ",";
+    stream << "mean,variance,standard deviation\n";
+    stream << to_string();
   }
 
+  std::string       name  ;
   std::vector<type> values;
 };
 
 template <typename type = double>
 struct session
 {
-  void to_csv(const std::string& filepath, const bool include_name = true)
+  virtual ~session() = default;
+
+  virtual std::string to_string()
   {
-    std::ofstream file(filepath);
-    file.precision(std::numeric_limits<type>::max_digits10);
-
-    for (auto& kvp : records)
-    {
-      if (include_name) file << kvp.first << ", ";
-      
-      auto& values = kvp.second.values;
-      auto  size   = values.size();
-
-      for (auto j = 0; j < size; ++j)
-      {
-        file << values[j];
-        if (j != size - 1) 
-          file << ", ";
-      }
-      
-      file << "\n";
-    }
+    std::ostringstream stream;
+    for (auto& record : records)
+      stream << record.to_string() << "\n";
+    return stream.str();
+  }
+  virtual void        to_csv   (const std::string& filepath)
+  {
+    std::ofstream stream(filepath);
+    stream << "name,";
+    for (auto i = 0; i < records[0].values.size(); ++i)
+      stream << "iteration " << i << ",";
+    stream << "mean,variance,standard deviation\n";
+    stream << to_string();
   }
 
-  std::vector<std::pair<std::string, record<type>>> records;
+  std::vector<record<type>> records;
 };
+
+#ifdef BM_MPI_SUPPORT
+template <typename type = double>
+class  mpi_session : public session<type>
+{
+public:
+  mpi_session           (MPI_Comm communicator = MPI_COMM_WORLD, std::int32_t master_rank = 0) : communicator_(communicator), master_rank_(master_rank)
+  {
+    MPI_Comm_rank(communicator_, &rank_);
+    MPI_Comm_size(communicator_, &size_);
+  }
+  mpi_session           (const mpi_session&  that) = default;
+  mpi_session           (      mpi_session&& temp) = default;
+ ~mpi_session           ()                         = default;
+  mpi_session& operator=(const mpi_session&  that) = default;
+  mpi_session& operator=(      mpi_session&& temp) = default;
+  
+  void                gather   ()
+  {
+    std::ostringstream stream;
+    for (auto& record : records) 
+      stream << rank_ << "," << record.to_string() << "\n";
+    std::string  local_string = stream      .str ();
+    std::int32_t local_size   = local_string.size();
+    
+    std::vector<std::int32_t> sizes        (size_);
+    std::vector<std::int32_t> displacements(size_);
+    std::int32_t              counter = 0;
+    MPI_Gather (&local_size, 1, MPI_INT, sizes.data(), 1, MPI_INT, master_rank_, communicator_);
+    for (auto i = 0; i < size_; ++i)
+      displacements[i] = counter, counter += sizes[i];
+    gathered_.resize(counter);
+    MPI_Gatherv(local_string.data(), local_string.size(), MPI_CHAR, gathered_.data(), sizes.data(), displacements.data(), MPI_CHAR, master_rank_, communicator_);
+  }
+  virtual std::string to_string()                            override
+  {
+    return rank_ == master_rank_ ? gathered_ : session<type>::to_string();
+  }
+  virtual void        to_csv   (const std::string& filepath) override
+  {
+    if (rank_ != master_rank_)
+      return;
+
+    std::ofstream stream(filepath);
+    stream << "rank,name,";
+    for (auto i = 0; i < records[0].values.size(); ++i)
+      stream << "iteration " << i << ",";
+    stream << "mean,variance,standard deviation\n";
+    stream << to_string();
+  }
+  
+protected:
+  MPI_Comm     communicator_;
+  std::int32_t master_rank_ ;
+  std::int32_t rank_        ;
+  std::int32_t size_        ;
+  std::string  gathered_    ;
+};
+#endif
 
 template <typename type = double, typename period = std::milli>
 class  session_recorder
@@ -90,7 +158,7 @@ public:
   }
   session_recorder           (const session_recorder&  that) = delete ;
   session_recorder           (      session_recorder&& temp) = default;
-  virtual ~session_recorder  ()                      = default;
+  virtual ~session_recorder  ()                              = default;
   session_recorder& operator=(const session_recorder&  that) = delete ;
   session_recorder& operator=(      session_recorder&& temp) = default;
   
@@ -100,16 +168,14 @@ public:
     function();
     const auto end   = std::chrono::high_resolution_clock::now();
 
-    // There isn't a cleaner way to do this without boost::multi_index...
-    // ...which is an unacceptible dependency for ~200 lines of code.
-    auto record = std::find_if(session_.records.begin(), session_.records.end(), 
-      [&name] (const std::pair<std::string, bm::record<type>>& record) { return record.first == name; });
+    auto record = std::find_if(session_.records.begin(), session_.records.end(),
+      [&name] (const bm::record<type>& record) { return record.name == name; });
     if (record == session_.records.end())
     {
       session_.records.push_back({name, {std::vector<type>(iterations_)}});
       record = std::prev(session_.records.end());
     }
-    record->second.values[index_] = std::chrono::duration<type, period>(end - start).count();
+    record->values[index_] = std::chrono::duration<type, period>(end - start).count();
   }
 
 protected:
@@ -119,9 +185,9 @@ protected:
 };
 
 template<typename type = double, typename period = std::milli>
-record<type>  run(const std::size_t iterations, const std::function<void()>&                                function)
+record<type>      run    (const std::function<void()>&                                function, const std::size_t iterations = 1)
 {
-  record<type> record {std::vector<type>(iterations)};
+  record<type> record {"", std::vector<type>(iterations)};
   for (auto i = 0; i < iterations; ++i)
   {
     const auto start = std::chrono::high_resolution_clock::now();
@@ -132,7 +198,7 @@ record<type>  run(const std::size_t iterations, const std::function<void()>&    
   return record;
 }
 template<typename type = double, typename period = std::milli>
-session<type> run(const std::size_t iterations, const std::function<void(session_recorder<type, period>&)>& function)
+session<type>     run    (const std::function<void(session_recorder<type, period>&)>& function, const std::size_t iterations = 1)
 {
   session<type> session;
   for(auto i = 0; i < iterations; ++i)
@@ -142,17 +208,19 @@ session<type> run(const std::size_t iterations, const std::function<void(session
   }
   return session;
 }
-
+#ifdef BM_MPI_SUPPORT
 template<typename type = double, typename period = std::milli>
-type          run(                              const std::function<void()>&                                function)
+mpi_session<type> run_mpi(const std::function<void(session_recorder<type, period>&)>& function, const std::size_t iterations = 1, const MPI_Comm communicator = MPI_COMM_WORLD, const std::int32_t master_rank = 0)
 {
-  return run<type, period>(std::size_t(1), function)[0];
+  mpi_session<type> session(communicator, master_rank);
+  for (auto i = 0; i < iterations; ++i)
+  {
+    session_recorder<type, period> recorder(i, iterations, session);
+    function(recorder);
+  }
+  return session;
 }
-template<typename type = double, typename period = std::milli>
-session<type> run(                              const std::function<void(session_recorder<type, period>&)>& function)
-{
-  return run<type, period>(std::size_t(1), function);
-}
+#endif
 }
 
 #endif
